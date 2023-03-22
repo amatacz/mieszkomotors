91adc7bf363ac1f8e17241bea4b842adc89bc548
amatacz <aleksandra.matacz93@gmail.com>
2023-03-22 15:31:03 +0100

    email templates updated

--- mieszkomotors/management/commands/send_email.py
+++ mieszkomotors/management/commands/send_email.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
         for car_event in car_events:
             subject = f"Zbliża się termin przeglądu dla auta {car_event[today]['license_plates']} właściciela {car_event[today]['owner_first_name']} {car_event[today]['owner_last_name']}"
 
-            html_body = render_to_string("../templates/car_email.html", car_event[today])
+            html_body = render_to_string("../templates/emails/car_email.html", car_event[today])
 
             email_from = settings.EMAIL_HOST_USER
             #recipient_list = settings.EMAIL_HOST_USER
@@ -39,7 +39,7 @@ class Command(BaseCommand):
         for insurance_event in insurance_events:
             subject = f"Zbliża się termin odnowienia ubezpieczenia dla auta {insurance_event[today]['car']} właściciela {insurance_event[today]['owner_first_name']} {insurance_event[today]['owner_last_name']}"
 
-            html_body = render_to_string("../templates/insurance_email.html", insurance_event[today])
+            html_body = render_to_string("../templates/emails/insurance_email.html", insurance_event[today])
 
             email_from = settings.EMAIL_HOST_USER
             #recipient_list = settings.EMAIL_HOST_USER
